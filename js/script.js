836241e3a56057855d25fe1fa952e486316a9db1
Sahil Makhijani <svmakhijani94@gmail.com>
2017-03-25 03:50:09 -0400

    Fixed comparator symbols

--- js/script.js
+++ js/script.js
@@ -16,105 +16,105 @@ $(function () {
         }
         // Gather states data
         $.each(data, function (key, value) {
-            if (value.state == 'AL')
+            if (value.state === 'AL')
                 states[0]++;
-            else if (value.state == 'AK')
+            else if (value.state === 'AK')
                 states[1]++;
-            else if (value.state == 'AZ')
+            else if (value.state === 'AZ')
                 states[2]++;
-            else if (value.state == 'AR')
+            else if (value.state === 'AR')
                 states[3]++;
-            else if (value.state == 'CA')
+            else if (value.state === 'CA')
                 states[4]++;
-            else if (value.state == 'CO')
+            else if (value.state === 'CO')
                 states[5]++;
-            else if (value.state == 'CT')
+            else if (value.state === 'CT')
                 states[6]++;
-            else if (value.state == 'DE')
+            else if (value.state === 'DE')
                 states[7]++;
-            else if (value.state == 'FL')
+            else if (value.state === 'FL')
                 states[8]++;
-            else if (value.state == 'GA')
+            else if (value.state === 'GA')
                 states[9]++;
-            else if (value.state == 'HI')
+            else if (value.state === 'HI')
                 states[10]++;
-            else if (value.state == 'ID')
+            else if (value.state === 'ID')
                 states[11]++;
-            else if (value.state == 'IL')
+            else if (value.state === 'IL')
                 states[12]++;
-            else if (value.state == 'IN')
+            else if (value.state === 'IN')
                 states[13]++;
-            else if (value.state == 'IA')
+            else if (value.state === 'IA')
                 states[14]++;
-            else if (value.state == 'KS')
+            else if (value.state === 'KS')
                 states[15]++;
-            else if (value.state == 'KY')
+            else if (value.state === 'KY')
                 states[16]++;
-            else if (value.state == 'LA')
+            else if (value.state === 'LA')
                 states[17]++;
-            else if (value.state == 'ME')
+            else if (value.state === 'ME')
                 states[18]++;
-            else if (value.state == 'MD')
+            else if (value.state === 'MD')
                 states[19]++;
-            else if (value.state == 'MA')
+            else if (value.state === 'MA')
                 states[20]++;
-            else if (value.state == 'MI')
+            else if (value.state === 'MI')
                 states[21]++;
-            else if (value.state == 'MN')
+            else if (value.state === 'MN')
                 states[22]++;
-            else if (value.state == 'MS')
+            else if (value.state === 'MS')
                 states[23]++;
-            else if (value.state == 'MO')
+            else if (value.state === 'MO')
                 states[24]++;
-            else if (value.state == 'MT')
+            else if (value.state === 'MT')
                 states[25]++;
-            else if (value.state == 'NE')
+            else if (value.state === 'NE')
                 states[26]++;
-            else if (value.state == 'NV')
+            else if (value.state === 'NV')
                 states[27]++;
-            else if (value.state == 'NH')
+            else if (value.state === 'NH')
                 states[28]++;
-            else if (value.state == 'NJ')
+            else if (value.state === 'NJ')
                 states[29]++;
-            else if (value.state == 'NM')
+            else if (value.state === 'NM')
                 states[30]++;
-            else if (value.state == 'NY')
+            else if (value.state === 'NY')
                 states[31]++;
-            else if (value.state == 'NC')
+            else if (value.state === 'NC')
                 states[32]++;
-            else if (value.state == 'ND')
+            else if (value.state === 'ND')
                 states[33]++;
-            else if (value.state == 'OH')
+            else if (value.state === 'OH')
                 states[34]++;
-            else if (value.state == 'OK')
+            else if (value.state === 'OK')
                 states[35]++;
-            else if (value.state == 'OR')
+            else if (value.state === 'OR')
                 states[36]++;
-            else if (value.state == 'PA')
+            else if (value.state === 'PA')
                 states[37]++;
-            else if (value.state == 'RI')
+            else if (value.state === 'RI')
                 states[38]++;
-            else if (value.state == 'SC')
+            else if (value.state === 'SC')
                 states[39]++;
-            else if (value.state == 'SD')
+            else if (value.state === 'SD')
                 states[40]++;
-            else if (value.state == 'TN')
+            else if (value.state === 'TN')
                 states[41]++;
-            else if (value.state == 'TX')
+            else if (value.state === 'TX')
                 states[42]++;
-            else if (value.state == 'UT')
+            else if (value.state === 'UT')
                 states[43]++;
-            else if (value.state == 'VT')
+            else if (value.state === 'VT')
                 states[44]++;
-            else if (value.state == 'VA')
+            else if (value.state === 'VA')
                 states[45]++;
-            else if (value.state == 'WA')
+            else if (value.state === 'WA')
                 states[46]++;
-            else if (value.state == 'WV')
+            else if (value.state === 'WV')
                 states[47]++;
-            else if (value.state == 'WI')
+            else if (value.state === 'WI')
                 states[48]++;
-            else if (value.state == 'WY')
+            else if (value.state === 'WY')
                 states[49]++;
         });
         // Create hc-key array using states data
